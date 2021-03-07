abd820e0c7c8e1476aa1b59380e33cd07fe40f1f
chc1129 <chc1129@gmail.com>
2021-03-07 10:51:10 +0900

    java prg fix

---
+++ chap03/Min2Cond1.java
@@ -0,0 +1,15 @@
+// 読み込んだ2つの整数値の小さいほうの値を表示 (条件演算子: その1)
+
+import java.util.Scanner;
+
+class Min2Cond1 {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("整数a: ");  int a = stdIn.nextInt();
+        System.out.print("整数b: ");  int b = stdIn.nextInt();
+
+        int min = a < b ? a : b;    // 小さいほうの値
+        System.out.println("小さいのは" + min + "です.");
+    }
+}
---
+++ chap03/Min2If.java
@@ -0,0 +1,20 @@
+// 読み込んだ2つの整数値の小さいほうの値を表示 (if文)
+
+import java.util.Scanner;
+
+class Min2If {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("整数a: ");  int a = stdIn.nextInt();
+        System.out.print("整数b: ");  int b = stdIn.nextInt();
+
+        int min;    // 小さいほうの値
+        if (a < b)
+            min = a;
+        else
+            min = b;
+
+        System.out.println("小さいのは" + min + "です.");
+    }
+}
