76220774dec730f897d4ab645ac1c4185ba00a06
chc1129 <chc1129@gmail.com>
2021-03-21 23:14:35 +0900

    java prg fix

---
+++ chap06/CopyArray.java
@@ -0,0 +1,24 @@
+
+import java.util.Arrays;
+
+class CopyArray {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {6, 5, 4, 3, 2, 1, 0};
+
+        System.out.println("a = " + Arrays.toString(a));
+        System.out.println("b = " + Arrays.toString(b));
+
+        if (a.length != b.length)
+            b = new int[a.length];
+
+        for (int i = 0; i < a.length; i++)
+            b[i] = a[i];
+
+        a[0] = 10; // 配列a[0]の値を書き換える
+
+        System.out.println("aをbに代入してa[0]に10を入れました.");
+        System.out.println("a = " + Arrays.toString(a));
+        System.out.println("b = " + Arrays.toString(b));
+    }
+}
